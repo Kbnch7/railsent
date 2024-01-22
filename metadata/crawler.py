29c0eede85749e76726e6479d83f5a8854ace6e0
alyaa17 <120140709+alyaa17@users.noreply.github.com>
2024-01-22 14:22:12 +0300

    Update crawler.py

--- metadata/crawler.py
+++ metadata/crawler.py
@@ -3,13 +3,16 @@ import requests
 from bs4 import BeautifulSoup
 
 data = set()
+link = input("Ссылка на сайт: ")
+html_class = input("Класс html: ")
+html_tag = input("Тэг html: ")  # p, a, div, etc
 
 
 def crawler():
-    response = requests.get('https://www.kp.ru/daily/23458/36682/')
+    response = requests.get(link)
     soup = BeautifulSoup(response.text, "html.parser")
 
-    result = soup.findAll("p", {"class": "sc-1wayp1z-16 dqbiXu"})
+    result = soup.findAll(html_tag, {"class": html_class})
     for elem in result:
         data.add(elem.getText())
 
@@ -18,3 +21,4 @@ def crawler():
 
 if __name__ == '__main__':
     crawler()
+    
